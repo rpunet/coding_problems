f0154d82d1928905203922bf5e215456f13cc9ed
rpunet <r.punetcorvillo@gmail.com>
2020-09-09 19:38:20 +0200

    Create version_numbers.c

---
+++ version_numbers.c
@@ -0,0 +1,32 @@
+int compareVersion(char * version1, char * version2)
+{
+    int i = 0;
+    int j = 0;
+    int num1 = 0;
+    int num2 = 0;
+    
+    while (version1[i] || version2[j])
+    {
+        while (version1[i] != '.' && version1[i] != '\0')
+        {
+            num1 = 10 * num1 + version1[i] - '0';
+            i++;
+        }
+        while (version2[j] != '.' && version2[j] != '\0')
+        {
+            num2 = 10 * num2 + version2[j] - '0';
+            j++;
+        }
+        if (num1 > num2)
+                return (1);
+        else if (num1 < num2)
+                return (-1);
+        num1 = 0;
+        num2 = 0;
+        if (version1[i] == '.')
+            i++;
+        if (version2[j] == '.')
+            j++;
+    }
+    return (0);
+}
