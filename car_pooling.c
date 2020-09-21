56def4a7572434b1368e060e2d6a78998d698a16
rpunet <r.punetcorvillo@gmail.com>
2020-09-21 11:35:00 +0200

    v1.0

---
+++ car_pooling.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   car_pooling.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpunet <rpunet@student.42madrid.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/19 11:03:30 by rpunet            #+#    #+#             */
+/*   Updated: 2020/09/19 19:36:31 by rpunet           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+bool carPooling(int** trips, int tripsSize, int* tripsColSize, int capacity)
+{
+    int trip_cap[1001] = {0};
+    int i;
+    int pick;
+    
+    i = 0;
+
+    
+    while (i < tripsSize)
+    {
+        pick = trips[i][1];
+        while (pick < trips[i][2])
+        {
+            trip_cap[pick] += trips[i][0];
+            if (trip_cap[pick] > capacity)
+                return (false);
+            pick++;
+        }
+        i++;
+    }
+    return (true);
+}
