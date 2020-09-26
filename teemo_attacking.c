1706d8b73c967d733ba574d4a25081c191e80c98
rpunet <r.punetcorvillo@gmail.com>
2020-09-26 12:04:50 +0200

    v1.0

---
+++ teemo_attacking.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   teemo_attacking.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpunet <rpunet@student.42madrid.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/23 13:22:30 by rpunet            #+#    #+#             */
+/*   Updated: 2020/07/27 22:07:16 by rpunet           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int ft_min(int a, int b)
+{
+    if (a < b)
+        return (a);
+    return (b);
+}
+
+int findPoisonedDuration(int* timeSeries, int timeSeriesSize, int duration)
+{
+    int i;
+    int tot;
+    
+    if (timeSeriesSize == 0)
+        return (0);
+    i = -1;
+    tot = 0;
+    while (i++ < (timeSeriesSize - 2))
+        tot += ft_min(timeSeries[i + 1] - timeSeries[i], duration);
+    return (tot + duration);
+}
